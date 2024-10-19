ba4dda9788ed3d906d3a4dbb4764b3585a211f8a
onur-ozkan <work@onurozkan.dev>
2024-10-19 18:00:29 +0300

    write more docs
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- src/entry.rs
+++ src/entry.rs
@@ -18,7 +18,15 @@ pub enum EntryStatus {
     ExpiresAtSeconds(u64),
 }
 
+/// Enum representing the status of an entry in the map.
+///
+/// - `Constant`: Entry is not expirable and remains accessible until removed.
+/// - `ExpiresAtSeconds`: Entry will expire once reached to the given time.
 impl EntryStatus {
+    /// Creates an instance based on the given duration.
+    ///
+    /// If `duration` is `Some`, the entry will be marked as expirable; otherwise,
+    /// it will be constant.
     fn from_duration<C: Clock>(clock: &C, duration: Option<Duration>) -> Self {
         match duration {
             Some(duration) => Self::ExpiresAtSeconds(
@@ -32,12 +40,17 @@ impl EntryStatus {
     }
 }
 
+/// The entry holds a value `V` and an associated `EntryStatus` which determines
+/// whether the entry is constant or expirable.
 pub(crate) struct ExpirableEntry<V> {
     value: V,
     status: EntryStatus,
 }
 
 impl<V> ExpirableEntry<V> {
+    /// Creates a new instance.
+    ///
+    /// If `duration` is `None`, entry will be constant/unexpirable.
     pub(crate) fn new<C: Clock>(clock: &C, v: V, duration: Option<Duration>) -> Self {
         Self {
             value: v,
@@ -53,10 +66,12 @@ impl<V> ExpirableEntry<V> {
         &self.value
     }
 
+    /// Returns owned `V` and consumes `self`.
     pub(crate) fn owned_value(self) -> V {
         self.value
     }
 
+    /// Checks if the entry has expired based on the current time.
     pub(crate) fn is_expired<C: Clock>(&self, clock: &C) -> bool {
         match self.status {
             EntryStatus::Constant => false,
@@ -66,6 +81,8 @@ impl<V> ExpirableEntry<V> {
         }
     }
 
+    /// Returns the remaining `Duration` before entry expires if it's expirable,
+    /// or `None` if it's constant.
     pub(crate) fn remaining_duration<C: Clock>(&self, clock: &C) -> Option<Duration> {
         match self.status {
             EntryStatus::Constant => None,
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
+//! Lightweight map implementation that supports expiring entries and fully
+//! compatible with both `std` and `no_std` environments.
+//!
+//! `TimedMap` allows storing key-value pairs with optional expiration times. Expiration is
+//! handled by an implementation of the `Clock` trait, which abstracts time handling for
+//! `no_std` environments.
+//!
+//! When `std` feature is enabled (which is the default case), `Clock` trait is handled
+//! automatically from the crate internals with `std::time::SystemTime`.
+//!
+//! ### Examples:
+//!
+//! #### In `std` environment:
+//! ```no_run
+//! use timed_map::TimedMap;
+//! use std::time::Duration;
+//!
+//! let mut map = TimedMap::new();
+//!
+//! map.insert(1, "value", Some(Duration::from_secs(60)));
+//! assert_eq!(map.get(&1), Some(&"value"));
+//! ```
+//!
+//! #### In `no_std` environment:
+//! ```no_run
+//! use timed_map::{Clock, TimedMap};
+//!
+//! struct CustomClock;
+//!
+//! impl Clock for CustomClock {
+//!     fn now_seconds(&self) -> u64 {
+//!         // Custom time implementation depending on the hardware.
+//!     }
+//! }
+//!
+//! let clock = CustomClock;
+//! let mut map = TimedMap::new(clock);
+//!
+//! map.insert(1, "value", None);
+//! assert_eq!(map.get(&1), Some(&"value"));
+//! ```
+
 #![no_std]
 
 #[cfg(feature = "std")]
