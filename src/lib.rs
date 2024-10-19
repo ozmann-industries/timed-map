6a8e5bb630165b9908f72621425a886d6370d53b
onur-ozkan <work@onurozkan.dev>
2024-10-19 15:31:53 +0300

    document `Clock` trait
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,44 @@ mod map;
 
 pub use map::TimedMap;
 
+/// Provides the current time in seconds.
+///
+/// This is designed to work across both `std` and `no_std` environments.
+///
+/// When compiled with `std` feature, a default implementation of the `now_seconds`
+/// method is provided, which returns the current system time in seconds since `UNIX_EPOCH`
+/// using `SystemTime`.
+///
+/// When `std` feature is disabled, user must implement the `now_seconds` method themselves
+/// typically using a custom time source (such as an embedded system's hardware timer).
+///
+/// # `std` example:
+/// ```rs
+/// use std::time::SystemTime;
+///
+/// struct MyClock;
+/// impl Clock for MyClock {}
+///
+/// let clock = MyClock;
+/// let current_time = clock.now_seconds();
+/// ```
+///
+/// # `no_std` example:
+/// ```rs
+/// struct CustomClock;
+///
+/// impl Clock for CustomClock {
+///     fn now_seconds(&self) -> u64 {
+///         // Custom implementation to retrieve the current time.
+///         0 // return a fixed dummy value for simplicity
+///     }
+/// }
+///
+/// let clock = CustomClock;
+/// let current_time = clock.now_seconds();
+/// ```
 pub trait Clock {
+    /// Returns the current time in seconds.
     #[cfg(feature = "std")]
     fn now_seconds(&self) -> u64 {
         use std::time::{SystemTime, UNIX_EPOCH};
@@ -19,6 +56,7 @@ pub trait Clock {
             .as_secs()
     }
 
+    /// Returns the current time in seconds.
     #[cfg(not(feature = "std"))]
     fn now_seconds(&self) -> u64;
 }
